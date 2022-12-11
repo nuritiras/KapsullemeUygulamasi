2114cbbeb7b051db33494bcde41a65b0261aeca3
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-12-11 23:17:29 +0300

    Proje dosyası ekle.

---
+++ KapsullemeUygulamasi/Personel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeUygulamasi
+{
+    public class Personel
+    {
+        private ulong tckimlikno;
+
+        public ulong TcKimlikNo
+        {
+            get
+            {
+                return tckimlikno / 1000000;
+            }
+
+            set
+            {
+                if (value >= 10000000000 && value < 100000000000)
+                {
+                    tckimlikno = value;
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz TC KİMLİK NO 11 karekter değidir.");
+                }
+            }
+        }
+    }
+}
---
+++ KapsullemeUygulamasi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel personel = new Personel();
+
+            Console.Write("TC Kimlik Numaranızı giriniz:"); string TCNO = Console.ReadLine();
+            if (ulong.TryParse(TCNO, out ulong tcno))
+            {
+                personel.TcKimlikNo = tcno;
+                Console.WriteLine("TC KİMLİK NO=" + personel.TcKimlikNo);
+            }
+            else
+            {
+                Console.WriteLine($"Geçersiz bir sayı girdiniz: {TCNO}");
+            }
+        }
+    }
+}
